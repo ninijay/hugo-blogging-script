ea831b586ac69030260a1b35017c2d68e0a2012e
Nini Jay <Nini Jay>
2016-09-10 18:46:44 +0200

    added script

---
+++ deploy.ps1
@@ -0,0 +1,28 @@
+﻿Write-Host "Begin Deployment"
+$date = Get-Date
+Write-Host "Build n Push"
+Invoke-Expression -Command:hugo
+Invoke-Expression -Command:'cd public'
+Invoke-Expression -Command:'git add -A'
+Invoke-Expression -Command:'git commit -m "Update, $($date)"'
+Invoke-Expression -Command:'git push blog master'
+Write-Host "git pull over SSH"
+ForEach( $session in Get-SSHSession) {
+    Write-Host $session.Host
+    if($session.Host -eq "server")
+    {
+        $mysession = $session
+    }
+    else
+    {
+        $mysession = -1
+    }
+ 
+ } 
+if($mysession -eq -1 -OR !$mysession)
+{
+    $mysession = New-SSHSession -ComputerName server -Credential (Get-Credential)
+}
+$result = Invoke-SSHCommand -Index $mysession.SessionId -Command "cd ~/www/ && git pull blog master"
+Write-Host "Status" $result.ExitStatus
+Pause
